1e7b20553adc1bc90d675b4b6d330df0581984cb
hendersonkleber <hendersonkleber@gmail.com>
2026-02-04 15:24:57 -0300

    feat: add product repository

---
+++ src/main/java/com/hendersonkleber/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.hendersonkleber.product.repository;
+
+import com.hendersonkleber.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
